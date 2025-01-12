3c185556a9064e7614a4de3208517a20498c915f
ElJDragon <el.jdragon@gmail.com>
2025-01-12 17:00:17 -0500

    Prueba no pull

--- models/conexion.php
+++ models/conexion.php
@@ -1,12 +1,12 @@
 <?php
 class conexion{
     public function conectar(){
-        $servername="bhlut4j4kuhxsivd4hrp-mysql.services.clever-cloud.com";
-        $username="uvhyhfvxsavrkktw";
-        $password="2w5zMYBaRxgZ5aeDVXTw";
-        $dbname="bhlut4j4kuhxsivd4hrp";
+        $servername=getenv('servername');
+        $username=getenv('username');
+        $password=getenv('password');
+        $dbname=getenv('dbname');
         $conn=mysqli_connect($servername,$username,$password,$dbname);
-        if(!$conn){
+        if(!$conn ){
             echo("Error en la conexion".mysqli_connect_error());
         }else{
             return $conn;
--- views/plantilla.php
+++ views/plantilla.php
@@ -37,8 +37,8 @@
 
     <section>
         <?php
-        require_once "../controllers/controller.php";
-        require_once "../models/model.php";
+        require_once "controllers/controller.php";
+        require_once "models/model.php";
         $mvc=new MvcController();
         $mvc->enlacesPaginasController();
         ?>
