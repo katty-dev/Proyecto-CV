34284837d31ac92bc832536a88e32b39feefd807
ElJDragon <el.jdragon@gmail.com>
2025-01-12 17:26:28 -0500

    conexcion

--- models/conexion.php
+++ models/conexion.php
@@ -1,14 +1,15 @@
 <?php
 class conexion{
     public function conectar(){
-        $servername=getenv('servername');
-        $username=getenv('username');
-        $password=getenv('password');
-        $dbname=getenv('dbname');
+        $servername=getenv("bhlut4j4kuhxsivd4hrp-mysql.services.clever-cloud.com");
+        $username=getenv("uvhyhfvxsavrkktw");
+        $password=getenv("2w5zMYBaRxgZ5aeDVXTw");
+        $dbname=getenv("bhlut4j4kuhxsivd4hrp");
         $conn=mysqli_connect($servername,$username,$password,$dbname);
         if(!$conn ){
             echo("Error en la conexion".mysqli_connect_error());
         }else{
+            echo("conectado")
             return $conn;
         }
      
